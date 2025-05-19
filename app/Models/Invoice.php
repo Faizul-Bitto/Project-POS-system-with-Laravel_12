9f729429627f1be092a2d365c6f657a9fbb9740f
Faizul Islam Bhuiyan (Bitto) <fibhuiyanbitto06@gmail.com>
2025-05-20 00:11:54 +0600

    add relationships for customers in Invoice model and products in InvoiceProduct model

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -3,7 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Invoice extends Model {
     protected $fillable = ['total', 'discount', 'vat', 'payable', 'user_id', 'customer_id'];
+
+    public function customers(): BelongsTo {
+        return $this->belongsTo( Customer::class );
+    }
 }
--- app/Models/InvoiceProduct.php
+++ app/Models/InvoiceProduct.php
@@ -3,7 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class InvoiceProduct extends Model {
     protected $fillable = ['invoice_id', 'product_id', 'quantity', 'price'];
+
+    public function product(): BelongsTo {
+        return $this->belongsTo( Product::class );
+    }
 }
