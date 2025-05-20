551d1871d7fa260abd65bf9305a3be9736559731
Faizul Islam Bhuiyan (Bitto) <fibhuiyanbitto06@gmail.com>
2025-05-20 23:43:32 +0600

    declare all methods for each feature for User in UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,37 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class UserController extends Controller
-{
-    //
+class UserController extends Controller {
+
+    public function userRegistration( Request $request ) {
+
+    }
+
+    public function userLogin( Request $request ) {
+
+    }
+
+    public function userLogout( Request $request ) {
+
+    }
+
+    public function sendOTP( Request $request ) {
+
+    }
+
+    public function verifyOTP( Request $request ) {
+
+    }
+
+    public function resetPassword( Request $request ) {
+
+    }
+
+    public function userProfileDetails( Request $request ) {
+
+    }
+
+    public function userProfileUpdate( Request $request ) {
+
+    }
 }
