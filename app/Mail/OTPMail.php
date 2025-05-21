b7764a22ec41ddfe874f0dcd7312839c8acc6b12
Faizul Islam Bhuiyan (Bitto) <fibhuiyanbitto06@gmail.com>
2025-05-21 22:42:43 +0600

    configure OTP mail

--- app/Mail/OTPMail.php
+++ app/Mail/OTPMail.php
@@ -3,41 +3,38 @@
 namespace App\Mail;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
-use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Mail\Mailables\Envelope;
 
-class OTPMail extends Mailable
-{
+class OTPMail extends Mailable {
     use Queueable, SerializesModels;
+
+    public $otp;
 
     /**
      * Create a new message instance.
      */
-    public function __construct()
-    {
-        //
+    public function __construct() {
+        $this->otp = $otp;
     }
 
     /**
      * Get the message envelope.
      */
-    public function envelope(): Envelope
-    {
+    public function envelope(): Envelope {
         return new Envelope(
-            subject: 'O T P Mail',
+            subject: 'PHP Laravel POS System Mail OTP',
         );
     }
 
     /**
      * Get the message content definition.
      */
-    public function content(): Content
-    {
+    public function content(): Content {
         return new Content(
-            view: 'view.name',
+            view: 'email.OTPMailTemplate',
         );
     }
 
@@ -46,8 +43,7 @@ class OTPMail extends Mailable
      *
      * @return array<int, \Illuminate\Mail\Mailables\Attachment>
      */
-    public function attachments(): array
-    {
+    public function attachments(): array {
         return [];
     }
 }
