5cbf13f985c5a72af20b410f602553ea65208cc4
Faizul Islam Bhuiyan (Bitto) <fibhuiyanbitto06@gmail.com>
2025-05-21 23:19:32 +0600

    remove logout route for api

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,5 @@ use App\Http\Controllers\UserController;
 
 Route::POST( '/user-registration', [UserController::class, 'userRegistration'] )->name( 'userRegistration' );
 Route::POST( '/user-login', [UserController::class, 'userLogin'] )->name( 'userLogin' );
-Route::POST( '/user-logout', [UserController::class, 'userLogout'] )->name( 'userLogout' );
 Route::POST( '/send-otp', [UserController::class, 'sendOTP'] )->name( 'sendOTP' );
 Route::POST( '/verify-otp', [UserController::class, 'verifyOTP'] )->name( 'verifyOTP' );
