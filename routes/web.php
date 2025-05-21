4b53d58b78938b746e6e9ab6438db1bdffe10751
Faizul Islam Bhuiyan (Bitto) <fibhuiyanbitto06@gmail.com>
2025-05-21 22:31:25 +0600

    recheck routes for both web and api for userLogout()

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
+Route::get( '/', function () {
+    return view( 'welcome' );
+} );
+
 Route::POST( '/user-registration', [UserController::class, 'userRegistration'] )->name( 'userRegistration' );
 Route::POST( '/user-login', [UserController::class, 'userLogin'] )->name( 'userLogin' );
 Route::POST( '/user-logout', [UserController::class, 'userLogout'] )->name( 'userLogout' );
